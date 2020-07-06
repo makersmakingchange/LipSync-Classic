cb024022b697b8e5e48ee18e42376b718f1be792
milad7 <develop@milad.mobi>
2020-07-05 21:52:18 -0700

    Corrected Settings commands

--- Software/LipSync_Firmware/LipSync_Firmware.ino
+++ Software/LipSync_Firmware/LipSync_Firmware.ino
@@ -964,10 +964,10 @@ void writeSettings(String changeString) {
       delay(5);
     } 
      //Get cursor initialization values if received "IN,0:0" and perform cursor initialization if received "IN,1:1"
-     else if(changeChar[0]=='I' && changeChar[1]=='N' && changeChar[3]=='0' && changeChar[3]=='0' && changeString.length()==4) {
+     else if(changeChar[0]=='I' && changeChar[1]=='N' && changeChar[2]=='0' && changeChar[3]=='0' && changeString.length()==4) {
       getCursorInitialization();
       delay(5);
-    } else if (changeChar[0]=='I' && changeChar[1]=='N' && changeChar[3]=='1' && changeChar[3]=='1' && changeString.length()==4) {
+    } else if (changeChar[0]=='I' && changeChar[1]=='N' && changeChar[2]=='1' && changeChar[3]=='1' && changeString.length()==4) {
       setCursorInitialization(2,true);
       delay(5);
     } 
