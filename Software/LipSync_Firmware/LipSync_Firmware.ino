be4dca6bfd9b08b4d88658aec8f74700801ed893
Jake McIvor <jake.mcivor@gmail.com>
2021-05-21 15:07:32 -0600

    Update version

--- Software/LipSync_Firmware/LipSync_Firmware.ino
+++ Software/LipSync_Firmware/LipSync_Firmware.ino
@@ -15,7 +15,7 @@
 
 //Developed by : MakersMakingChange
 //Firmware : LipSync_Firmware
-//VERSION: 2.71.1 (01 Apr 2021) 
+//VERSION: 3.0-beta (21 May Apr 2021) 
 //Copyright Neil Squire Society 2021.
 
 
